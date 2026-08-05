113cdb23b86c57248a68a05dd962ece61ef81913
KHEDIR33 <internationalkdr33@gmail.com>
2026-08-05 11:30:08 +0300

    Add payment routes for creating payments

---
+++ backend/src/routes/paymentRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+const paymentController =
+require("../controllers/paymentController");
+
+
+
+router.post(
+    "/create",
+    paymentController.createPayment
+);
+
+
+
+module.exports = router;
