18e69f69faded461afbd4c2ac957435da750a7ec
KHEDIR33 <internationalkdr33@gmail.com>
2026-08-05 13:51:14 +0300

    Add UploadRoutes for file upload functionality

---
+++ backend/src/routes/UploadRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+const upload = require("../config/multer");
+const { uploadFile } = require("../controllers/uploadController");
+
+
+router.post(
+    "/",
+    upload.single("file"),
+    uploadFile
+);
+
+
+module.exports = router;
