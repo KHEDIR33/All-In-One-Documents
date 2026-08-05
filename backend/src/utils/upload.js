3d8e9e26c84cbf4f988db9c953bc0abc8293bb28
KHEDIR33 <internationalkdr33@gmail.com>
2026-08-05 10:26:09 +0300

    Create upload.js

---
+++ backend/src/utils/upload.js
@@ -0,0 +1,43 @@
+const multer = require("multer");
+const path = require("path");
+
+
+const storage = multer.diskStorage({
+
+    destination:(req,file,cb)=>{
+
+        cb(null,"uploads/");
+
+    },
+
+
+    filename:(req,file,cb)=>{
+
+        const uniqueName =
+        Date.now()+"-"+file.originalname;
+
+
+        cb(null,uniqueName);
+
+    }
+
+});
+
+
+const upload = multer({
+
+    storage,
+
+
+    limits:{
+
+        fileSize:
+        50 * 1024 * 1024
+
+    }
+
+
+});
+
+
+module.exports = upload;
